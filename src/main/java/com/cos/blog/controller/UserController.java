c8bf6d1d12eee960feedc3d3a1d345ee65bb2de7
jooyoun lee <115621310+zwzwzw123@users.noreply.github.com>
2023-02-03 15:33:32 +0900

    카카오 로그인 서비스 구현

--- src/main/java/com/cos/blog/controller/UserController.java
+++ src/main/java/com/cos/blog/controller/UserController.java
@@ -1,8 +1,16 @@
 package com.cos.blog.controller;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 import com.cos.blog.config.auth.PrincipalDetail;
 
@@ -27,5 +35,38 @@ public class UserController {
 		return "user/updateForm";
 	}
 
-	
+	@GetMapping("/auth/kakao/callback")
+	public @ResponseBody String kakaoCallback(String code) {
+		
+		//post방식으로 key-value데이터를 요청(카카오쪽으로) 
+		//라이브러리 : Retrofit2, OkHttp, RestTemplate
+		
+		RestTemplate rt = new RestTemplate();
+		
+		//HttHeader 오브젝트 생성
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+		
+		//HttpBody 오브젝트 생성
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("grant_type", "authorization_code");
+		params.add("client_id", "8df17247d55783bd03518a5c7e50d6e4");
+		params.add("redirect_uri", "http://localhost:8000/auth/kakao/callback"	);
+		params.add("code", code);
+		
+		//HttpHeader와 HttpBody를 하나의 오브젝트에 담기
+		HttpEntity<MultiValueMap<String , String>> kakaoTokenRequest=
+				new HttpEntity<>(params, headers);
+		
+		//Http요청하기 - Post방식으로 - response변수의 응답을 받음
+		ResponseEntity<String> response = rt.exchange(
+				"https://kauth.kakao.com/oauth/token",
+				HttpMethod.POST,
+				kakaoTokenRequest,
+				String.class
+				);
+				
+		return "카카오 토큰 인증 완료 : 토큰요청에 대한 응답 "+response;
+		
+	}
 }
--- src/main/webapp/WEB-INF/views/user/loginForm.jsp
+++ src/main/webapp/WEB-INF/views/user/loginForm.jsp
@@ -4,16 +4,16 @@
 
 <div class="container">
 
-	<form action="/auth/loginProc"  method="post">
+	<form action="/auth/loginProc" method="post">
 		<div class="form-group">
-			<label for="username">Username</label> 
-			<input name="username" type="text" class="form-control" placeholder="Enter username" id="username">
+			<label for="username">Username</label> <input name="username" type="text" class="form-control" placeholder="Enter username" id="username">
 		</div>
 		<div class="form-group">
-			<label for="password">Password</label> 
-			<input type="password" name="password" class="form-control" placeholder="Enter password" id="password">
+			<label for="password">Password</label> <input type="password" name="password" class="form-control" placeholder="Enter password" id="password">
 		</div>
 		<button id="btn-login" class="btn btn-primary">로그인</button>
+		<a href=" https://kauth.kakao.com/oauth/authorize?client_id=8df17247d55783bd03518a5c7e50d6e4&redirect_uri=http://localhost:8000/auth/kakao/callback&response_type=code"><img height="39px"
+			src="/image/kakao_login_button.png"></a>
 	</form>
 
 </div>
